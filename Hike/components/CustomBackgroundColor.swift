aabed46f87394d118c4986d5c132a404179d0e1e
baris <bdilekci200@gmail.com>
2023-09-22 22:57:17 +0300

    feat: create cardview and custombackgroundview

---
+++ Hike/components/CustomBackgroundColor.swift
@@ -0,0 +1,18 @@
+//
+//  CustomBackgroundColor.swift
+//  Hike
+//
+//  Created by Baris Dilekci on 22.09.2023.
+//
+
+import SwiftUI
+
+struct CustomBackgroundColor: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CustomBackgroundColor()
+}
---
+++ Hike/extensions/UIColor+Extensions.swift
@@ -0,0 +1,8 @@
+//
+//  UIColor+Extensions.swift
+//  Hike
+//
+//  Created by Baris Dilekci on 22.09.2023.
+//
+
+import Foundation
