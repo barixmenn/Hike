09ef56ea365d921c535ad982fdf6a8178ccdeafc
baris <bdilekci200@gmail.com>
2023-09-22 22:41:19 +0300

    Initial Commit

---
+++ Hike/HikeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HikeApp.swift
+//  Hike
+//
+//  Created by Baris Dilekci on 22.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct HikeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
