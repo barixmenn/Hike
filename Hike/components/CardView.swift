29f3baf69f4289cf69b4413ff6dabf35cae0a1f3
baris <bdilekci200@gmail.com>
2023-09-22 23:12:51 +0300

    fix

--- Hike/components/CardView.swift
+++ Hike/components/CardView.swift
@@ -9,7 +9,47 @@ import SwiftUI
 
 struct CardView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        //MARK: - CARD
+        ZStack {
+            CustomBackgroundView()
+            VStack {
+                //MARK: - HEADER
+                VStack(alignment: .leading, content: {
+                    HStack {
+                        Text("Hiking")
+                            .fontWeight(.black)
+                            .font(.system(size: 52))
+                            .foregroundStyle(
+                                LinearGradient(colors: [.customGrayLight, .customGrayMedium], startPoint: .top, endPoint: .bottom)
+                        )
+                        Spacer()
+        
+                        Button{
+                            print("clicked")
+                        } label : {
+                            CustomButtonView()
+                        }
+                    }
+                    Text("Fun and enjoyable outdoor activity for friends and families")
+                        .multilineTextAlignment(.leading)
+                        .italic()
+                        .foregroundColor(.customGrayMedium)
+                        
+                        
+                }).padding(.horizontal, 30)
+                
+                //MARK: - CONTENT
+                ZStack{
+                    Circle()
+                        .fill(LinearGradient(colors: [Color("ColorIndigoMedium"),Color("ColorSalmonLight")] ,startPoint: .topLeading, endPoint: .bottomTrailing))
+                        .frame(width: 256, height: 256)
+                    
+                    Image("image-1")
+                        .resizable()
+                        .scaledToFit()
+                }
+            }
+        }.frame(width: 320,height: 570)
     }
 }
 
--- Hike/components/CustomBackgroundColor.swift
+++ Hike/components/CustomBackgroundColor.swift
@@ -7,12 +7,38 @@
 
 import SwiftUI
 
-struct CustomBackgroundColor: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+struct CustomBackgroundView: View {
+  var body: some View {
+    ZStack {
+      // MARK: - 3. DEPTH
+      
+      Color.customGreenDark
+        .cornerRadius(40)
+        .offset(y: 12)
+      
+      // MARK: - 2. LIGHT
+      
+      Color.customGreenLight
+        .cornerRadius(40)
+        .offset(y: 3)
+        .opacity(0.85)
+      
+      // MARK: - 1. SURFACE
+      
+      LinearGradient(
+        colors: [
+          .customGreenLight,
+          .customGreenMedium],
+        startPoint: .top,
+        endPoint: .bottom
+      )
+      .cornerRadius(40)
     }
+  }
 }
 
+
+
 #Preview {
-    CustomBackgroundColor()
+    CustomBackgroundView()
 }
