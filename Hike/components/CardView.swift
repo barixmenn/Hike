0a801bf68713eb5f5eb639704b25867072f35684
baris <bdilekci200@gmail.com>
2023-09-23 15:00:33 +0300

    feat: create random ımage button

--- Hike/components/CardView.swift
+++ Hike/components/CardView.swift
@@ -8,6 +8,21 @@
 import SwiftUI
 
 struct CardView: View {
+    //MARK: - PROPERTIES
+    @State private var imageNumber : Int = 1
+    @State private var randomNumber : Int = 1
+    
+    //MARK: - FUNCTIONS
+    func randomImage() {
+        
+        repeat {
+            randomNumber = Int.random(in: 1...5)
+
+        } while randomNumber == imageNumber
+        imageNumber = randomNumber
+        
+    }
+    
     var body: some View {
         //MARK: - CARD
         ZStack {
@@ -44,11 +59,36 @@ struct CardView: View {
                         .fill(LinearGradient(colors: [Color("ColorIndigoMedium"),Color("ColorSalmonLight")] ,startPoint: .topLeading, endPoint: .bottomTrailing))
                         .frame(width: 256, height: 256)
                     
-                    Image("image-1")
+                    Image("image-\(imageNumber)")
                         .resizable()
                         .scaledToFit()
-                }
-            }
+                        .animation(.easeInOut(duration: 3), value: imageNumber)
+                 
+                    
+                } //: ZSTACK
+                
+                //MARK: - FOOTER
+                
+                Button {
+                    print("random image button ")
+                    randomImage()
+                } label: {
+                    Text("Explore More")
+                      .font(.title2)
+                      .fontWeight(.heavy)
+                      .foregroundStyle(
+                        LinearGradient(
+                          colors: [
+                            .customGreenLight,
+                            .customGreenMedium
+                          ],
+                          startPoint: .top,
+                          endPoint: .bottom
+                        )
+                      )
+                      .shadow(color: .black.opacity(0.25), radius: 0.25, x: 1, y: 2)
+                }.buttonStyle(GradientButton())
+            } //: CARD
         }.frame(width: 320,height: 570)
     }
 }
